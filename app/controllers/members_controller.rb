6931fd28c9ccf2f16aa175a1bf2ede8b1427fb35
Rayman Yan <ryan@aligntech.com>
2024-01-18 11:16:51 +0800

    episode16

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,9 +1,8 @@
 class MembersController < ApplicationController
-  before_action :authenticate_user!, only: %i[edit_description update_description]
-
+  before_action :authenticate_user!, only: %i[edit_description update_description edit_details update_details]
 
   def show
-    @user = User.find(params[:id]);
+    @user = User.find(params[:id])
   end
 
   def edit_description; end
@@ -11,18 +10,31 @@ class MembersController < ApplicationController
   def update_description
     respond_to do |format|
       if current_user.update(about: params[:user][:about])
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description', locals: { user: current_user }) }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('member-description', partial: 'members/member_description',
+                                                                          locals: { user: current_user })
+        end
       end
     end
   end
 
   def edit_details; end
 
-  def update_details 
+  def update_details
     respond_to do |format|
-      if current_user.update(first_name: params[:user][:first_name], last_name: params[:user][:last_name], city: params[:user][:city], country: params[:user][:country], state: params[:user][:state] ,pincode: params[:user][:pincode] ,profile_title: params[:user][:profile_title])
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('member-details', partial: 'members/member_details', locals: { user: current_user }) }
+      if current_user.update(details_params)
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace('member-details', partial: 'members/member_details',
+                                                                      locals: { user: current_user })
+        end
       end
     end
+  end
+
+  private
+
+  def details_params
+    params.require(:user).permit(:first_name, :last_name, :city, :state, :country, :pincode, :profile_title,
+                                 :street_address)
   end
 end
