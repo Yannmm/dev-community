4d45cc5e952ca343d390300ceb2613104d2c7e87
Rayman Yan <ryan@aligntech.com>
2024-01-19 16:07:24 +0800

    expisode22

--- app/models/work_experience.rb
+++ app/models/work_experience.rb
@@ -2,4 +2,32 @@ class WorkExperience < ApplicationRecord
   belongs_to :user # as singular
 
   validates :company, :start_date, :employment_type, :job_title, :location, :location_type, presence: true
+
+  validate :work_experience_last_date, :presence_of_end_date
+
+  validate :end_date_greater_than_start_date, if: :currently_not_working_here?
+
+  def work_experience_last_date
+    return unless end_date.present? && currently_working_here
+
+    errors.add(:end_date, 'must be blank if you are currently working in this company')
+  end
+
+  def presence_of_end_date
+    return unless end_date.blank? && !currently_working_here
+
+    errors.add(:end_date, 'must be present if you are not currently working in this company')
+  end
+
+  def currently_not_working_here?
+    !currently_working_here
+  end
+
+  def end_date_greater_than_start_date
+    return if end_date.nil?
+
+    return unless end_date < start_date
+
+    errors.add(:end_date, 'must be greater than start_date')
+  end
 end
