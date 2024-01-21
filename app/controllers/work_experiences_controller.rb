dc13c064181a250695379bb4d048f923ec63c1f8
Yan Meng <yannmm@foxmail.com>
2024-01-21 20:39:35 +0800

    episode28

--- app/controllers/work_experiences_controller.rb
+++ app/controllers/work_experiences_controller.rb
@@ -32,6 +32,10 @@ class WorkExperiencesController < ApplicationController
   end
 
   def destroy 
+    respond_to do |format|
+      @work_experience.destroy
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(%Q[work_experience_item_#{@work_experience.id}]) }
+    end
   end
 
   private
