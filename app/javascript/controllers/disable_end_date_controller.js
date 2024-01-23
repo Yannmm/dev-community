1636c84d2cdff86c7929ba8300399db856fbf6e1
Yan Meng <yannmm@foxmail.com>
2024-01-23 21:55:18 +0800

    optimize end data disable logic.

--- app/javascript/controllers/disable_end_date_controller.js
+++ app/javascript/controllers/disable_end_date_controller.js
@@ -2,20 +2,24 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="disable-end-date"
 export default class extends Controller {
+  static targets = ["endDateField", "switch"];
+
   connect() {
     this.disableEndDate();
   }
 
   initialize() {
-    this.element.setAttribute(
-      "data-action",
-      "click->disable-end-date#disableEndDate"
-    );
+    // this.element.setAttribute(
+    //   "data-action",
+    //   "click->disable-end-date#disableEndDate"
+    // );
   }
 
   disableEndDate() {
-    const endDateElement = document.getElementById("work_experience_end_date");
-    if (this.element.checked) {
+    // const endDateElement = document.getElementById("work_experience_end_date");
+    const endDateElement = this.endDateFieldTarget;
+    const switch1 = this.switchTarget;
+    if (switch1.checked) {
       endDateElement.value = null;
       endDateElement.setAttribute("disabled", true);
     } else {
