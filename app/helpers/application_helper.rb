b0a8829ad7f0e662e7dd3a3cae89a062de2874b7
Yan Meng <yannmm@foxmail.com>
2024-01-27 21:20:05 +0800

    episode38

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def get_connection_status(current_user, user)
-    current_user.connections.find_by(connected_user_id: user.id).status
+    current_user.connections.find_by(connected_user_id: user.id)&.status
   end
 end
