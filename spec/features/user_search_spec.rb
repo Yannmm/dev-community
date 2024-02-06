c4cac7f153eb058153a4cce45db9ed9ebe07ffe5
Yan Meng <yannmm@foxmail.com>
2024-02-06 21:34:11 +0800

    episode63, cannot figure out how to use shared_exmaples

--- spec/features/user_search_spec.rb
+++ spec/features/user_search_spec.rb
@@ -13,30 +13,32 @@ RSpec.feature "UserSearches", type: :feature do
       @user5 = create(:user, country: 'China', city: 'Shanghai')
 
       sleep 1
-
+      
+      visit root_path
     end
 
     describe 'search by country' do 
       it 'should allow user to search other users by complete country name' do 
-        visit root_path 
+        
 
         fill_in 'q_city_or_country_cont', with: 'China'
-        click_button 'Serach'
+        click_button 'Search'
 
         expect(page).to have_text(@user5.name)
         expect(page).to have_text(@user5.profile_title)
         expect(page).to have_text(@user5.country)
 
         expect(page).to_not have_text(@user1.name)
-        expect(page).to_not have_text(@user2.profile_title)
+        expect(page).to_not have_text(@user2.name)
+        expect(page).to_not have_text(@user4.name)
         expect(page).to_not have_text(@user3.country)
       end 
 
       it 'should allow user to search other users by partial country name' do 
-        visit root_path 
+        
 
         fill_in 'q_city_or_country_cont', with: 'Aus'
-        click_button 'Serach'
+        click_button 'Search'
 
         expect(page).to have_text(@user2.name)
         expect(page).to have_text(@user2.profile_title)
@@ -48,10 +50,76 @@ RSpec.feature "UserSearches", type: :feature do
       end 
 
       it 'should allow user to search other users by any random character in the country name' do 
-        visit root_path 
+        
 
         fill_in 'q_city_or_country_cont', with: 'a'
-        click_button 'Serach'
+        click_button 'Search'
+
+        expect(page).to have_text(@user1.name)
+        expect(page).to have_text(@user1.profile_title)
+        expect(page).to have_text(@user1.country)
+
+        expect(page).to have_text(@user2.name)
+        expect(page).to have_text(@user2.profile_title)
+        expect(page).to have_text(@user2.country)
+
+        expect(page).to have_text(@user3.name)
+        expect(page).to have_text(@user3.profile_title)
+        expect(page).to have_text(@user3.country)
+
+        expect(page).to have_text(@user4.name)
+        expect(page).to have_text(@user4.profile_title)
+        expect(page).to have_text(@user4.country)
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.country)
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.country)
+
+      end 
+    end
+
+    describe 'search by city' do 
+      it 'should allow user to search other users by complete city name' do 
+        
+
+        fill_in 'q_city_or_country_cont', with: 'Shanghai'
+        click_button 'Search'
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.country)
+
+        expect(page).to_not have_text(@user1.name)
+        expect(page).to_not have_text(@user2.name)
+        expect(page).to_not have_text(@user4.name)
+        expect(page).to_not have_text(@user3.country)
+      end 
+
+      it 'should allow user to search other users by partial city name' do 
+        
+
+        fill_in 'q_city_or_country_cont', with: 'Shan'
+        click_button 'Search'
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.country)
+
+        expect(page).to_not have_text(@user1.name)
+        expect(page).to_not have_text(@user2.name)
+        expect(page).to_not have_text(@user4.name)
+        expect(page).to_not have_text(@user3.country)
+      end 
+
+      it 'should allow user to search other users by any random character in the city name' do 
+        
+
+        fill_in 'q_city_or_country_cont', with: 'n'
+        click_button 'Search'
 
         expect(page).to have_text(@user1.name)
         expect(page).to have_text(@user1.profile_title)
