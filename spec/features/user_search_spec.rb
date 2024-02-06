f677c73afae02c8449f907e7b3e4128a9aee85da
Yan Meng <yannmm@foxmail.com>
2024-02-06 21:44:22 +0800

    episode64

--- spec/features/user_search_spec.rb
+++ spec/features/user_search_spec.rb
@@ -147,5 +147,32 @@ RSpec.feature "UserSearches", type: :feature do
 
       end 
     end
+
+    describe 'view user profile' do 
+      it 'should search user and visit his profile' do 
+        fill_in 'q_city_or_country_cont', with: 'Shanghai'
+        click_button 'Search'
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.country)
+
+        expect(page).to_not have_text(@user1.name)
+        expect(page).to_not have_text(@user2.name)
+        expect(page).to_not have_text(@user4.name)
+        expect(page).to_not have_text(@user3.country)
+
+        click_link 'View Profile'
+        expect(page).to have_current_path("/member/#{@user5.id}")
+
+        sleep 2
+
+        expect(page).to have_text(@user5.name)
+        expect(page).to have_text(@user5.profile_title)
+        expect(page).to have_text(@user5.address)
+        expect(page).to have_text(@user5.about)
+        expect(page).to have_text('About')
+      end
+    end
   end
 end
